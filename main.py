aaf3cc08eabc931a8cc67e1fd37a26e20765e3fc
Greg TK <33130539+GregTCLTK@users.noreply.github.com>
2018-01-27 12:00:22 +0100

    Main Update Teil 1
    Main Update Teil 1

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-#imports
 import sys
 import discord
 import time
 
-#Files
 import KEYS
 
 class MyClient(discord.Client):
 	async def on_ready(self):
-		print("Login successful")
-		print("A list of all servers where the bot is on:")
-		#List of all Servers the Bot is
+		print("Erfolgreich eingeloggt!")
+		print("Der Bot ist auf folgeneden Servern:")
 		insguilds = 0
 		for guild in client.guilds:
 			print("%s - %s" %(guild.name, guild.id))
 			insguilds = insguilds + 1
-		print("Guilds: " + str(insguilds))
+		print("Server: " + str(insguilds))
 
 		#Game Precense
 		game = discord.Game(name="-help")
 		await client.change_presence(status=discord.Status.online, game=game)
 
 	async def on_message(self, message):
-		#you could define the Prefix in the File Keys
 		if message.content.startswith(KEYS.PREFIX):
 			invoke = message.content[1:].split(" ")[0]
 			args = message.content.split(" ")[1:]
@@ -34,30 +30,30 @@ class MyClient(discord.Client):
 				try:
 					ammount = int(args[0])+1 if len(args) > 0 else 1
 				except:
-					await message.channel.send(embed=discord.Embed(color=discord.Color.red(), description="UNKNOWN VALUE"))
+					await message.channel.send(embed=discord.Embed(color=discord.Color.red(), description="UNGÜLTIGER WERT"))
 					return
 				async for message in message.channel.history(limit=ammount):
 					try:
 						await message.delete()
 					except discord.error.Forbidden:
-						await message.channel.send(embed=discord.Embed(color=discord.Color.red(),description="OH it looks like that i do not have the permissions to delete messages here. I am sorry!"))
+						await message.channel.send(embed=discord.Embed(color=discord.Color.red(),description="OH es sieht so aus als hättest du keine Permissions um nachrichten zu löschen. Sorry"))
 						break
 
 			elif invoke=="clear" or invoke=="message" and message.author.guild_permissions.administrator == False:
-				notification = await message.channel.send(embed=discord.Embed(color=discord.Color.red(),description="This command can only use administrators, sorry").set_thumbnail(url="https://thebotdev.de/assets/img/alert.png"))
+				notification = await message.channel.send(embed=discord.Embed(color=discord.Color.red(),description="Diesen Command dürfen leider nur Administratoren nutzen, sorry").set_thumbnail(url="https://thebotdev.de/assets/img/alert.png"))
 				await asyncio.sleep(20)
 				await notification.delete()
 			if invoke == "help":
-				await message.channel.send(content="I sent you a private message with all the orders! If you haven't received anything, please check if you have enabled messages from server members. Also you could find help at https://thebotdev.de/work01.html  https://i.imgur.com/QFtGBSG.png")
+				await message.channel.send(content="Ich habe die eine Liste mit allen Commands per DM geschickt. Wenn du nicht bekommen hast überprüfe bitte ob du es aktiviert hast das du Direktnachrichten von Servermitgliedern aktiviert hast. Du findest Hilfe auch auf: https://thebotdev.de/work01.html  https://i.imgur.com/QFtGBSG.png")
 				try;
-					await message.author.send(embed=discord.Embed(description="***Support***\n**Prefix:**\n-[command]\n**Check if the bot is online**\n-ping \nshows that the bot is online and reads the messages\n**delete messages**\n-clear [ammount]\nDeletes the specified number of messages\n**Create a message**\n-message\nThere is a wizard that will help you to create the message as you like.\n**Bot Info**\n-info\n-----------\nFormatting of the message: *https://thebotdev.de/Markdown.html*\n"))
+					await message.author.send(embed=discord.Embed(description="***Support***\n**Prefix:**\n-[command]\n**Checkt ob der Bot online ist**\n-ping \nzeigt das der Bot online ist und die nachricht gelsesen hat\n*Nachricht löschen**\n-clear [Anzahl der nachrichten die gelöscht werden sollen]\nLöscht so viele Nachrichten wie nach dem Command angegeben sind\n**Erstellen einer Nachricht**\n-message\nDort ist ein Wizzard der dir dabei helfen wird die nachricht so zu erstellen wie du willst.\n**Bot Info**\n-info\n-----------\nFormatierung der Nachricht: *https://thebotdev.de/Markdown.html*\n"))
 				except discord.error.Forbidden:
-					await message.channel.send(content="Error! i couldnt send you the message per DM so here is the help command for you :)")
-					await message.author.send(embed=discord.Embed(description="***Support***\n**Prefix:**\n-[command]\n**Check if the bot is online**\n-ping \nshows that the bot is online and reads the messages\n**delete messages**\n-clear [ammount]\nDeletes the specified number of messages\n**Create a message**\n-message\nThere is a wizard that will help you to create the message as you like.\n**Bot Info**\n-info\n-----------\nFormatting of the message: *https://thebotdev.de/Markdown.html*\n"))
+					await message.channel.send(content="Error! Ich konnte dir leider keine nachricht per DM schicken deswegen ist jetzt hier die Hilfe Liste für dich :)")
+					await message.author.send(embed=discord.Embed(description="***Support***\n**Prefix:**\n-[command]\n**Checkt ob der Bot online ist**\n-ping \nzeigt das der Bot online ist und die nachricht gelsesen hat\n*Nachricht löschen**\n-clear [Anzahl der nachrichten die gelöscht werden sollen]\nLöscht so viele Nachrichten wie nach dem Command angegeben sind\n**Erstellen einer Nachricht**\n-message\nDort ist ein Wizzard der dir dabei helfen wird die nachricht so zu erstellen wie du willst.\n**Bot Info**\n-info\n-----------\nFormatierung der Nachricht: *https://thebotdev.de/Markdown.html*\n"))
 			if invoke == "info":
-				await message.channel.send(embed=discord.Embed(color=discord.Color.purple(), description="**Bot by:** *BaseChip*\n**Project:** *TheBotDev*\n**Support:** *[BaseChips support server](https://discord.gg/HD7x2vx)*\n**Bot Invite:** *[INVITE BaseChips Bot](https://discordapp.com/api/oauth2/authorize?client_id=384757717346025472&permissions=67577856&scope=bot)*\n*This is a fork from GitHub from the Bot from BaseChip*"))
+				await message.channel.send(embed=discord.Embed(color=discord.Color.purple(), description="**Bot von *BaseChip*\n**Projek:** *TheBotDev*\n**Support:** *[BaseChip's Support Server](https://discord.gg/HD7x2vx)*\n**Bot Invite:** *[INVITE BaseChip's Bot](https://discordapp.com/api/oauth2/authorize?client_id=384757717346025472&permissions=67577856&scope=bot)*\n*Dies ist ein Fork von GitHub von dem Bot von BaseChip*"))
 			if invoke == "message" and message.author.guild_permissions.administrator == True:
-				setup = await message.channel.send(embed=discord.Embed(color=discord.Color.magenta(), description="OK! The setup for creating a message is started. Please send now the color, which should have the border (green/red/magenta/blue/gold) and don't worry about this message and which ones will be written during setup I delete later:) "))
+				setup = await message.channel.send(embed=discord.Embed(color=discord.Color.magenta(), description="OK! Das Setup um die nachricht zu erstellen wurde gestartet. Bitte sende nun die Farbe die die Embed Message am Rand haben soll (Grün/Rot/Magenta/Blau/Gold) und mache dir keine Sorgen über die nachricht die während der Installation geschrieben werden ich werde diese spter löschen :)"))
 
 				def checkmsg(m):
 					if m.author.id == message.author.id and m.channel.id == message.channel.id:
@@ -67,7 +63,7 @@ class MyClient(discord.Client):
 				if msgwaitfor.content != " ":
 
 					if msgwaitfor.content == "green":
-						print("Color: Green")
+						print("Farbe: Grün")
 						cg = await message.channel.send(embed=discord.Embed(color=discord.Color.green(), description="You have chosen the color green. As an example of what the message looks like later, this message has already colored the border. So now please send me the text, which your message should have"))
 						def checkg(m):
 							if message.content != None and m.author.id == message.author.id and m.channel.id == message.channel.id:
